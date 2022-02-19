7f02bcd82073512619149145624492c56694cd63
thehappycheese <the.nicholas.archer@gmail.com>
2022-02-19 10:15:13 +0800

    disaster, this code is broken for unknown reasons

--- src/lookup.rs
+++ src/lookup.rs
@@ -1,4 +1,4 @@
-use bincode;
+// use bincode;
 
 use geo::line_interpolate_point::LineInterpolatePoint;
 use geo::line_locate_point::LineLocatePoint;
@@ -6,18 +6,26 @@ use geo::{point};
 use geo::prelude::EuclideanDistance;
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
-use pyo3::types::{PyBytes, PyDict, PyTuple, PyList};
+use pyo3::types::{
+    //PyBytes,
+    PyDict,
+    PyTuple,
+    PyList,
+};
 
 use rayon::prelude::*;
 
 
-use serde::{Serialize, Deserialize};
+// use serde::{Serialize, Deserialize};
 
-use crate::datatypes::{ExtractedFeature, RoadSectionsByCarriageway};
+use crate::datatypes::{
+    ExtractedFeature,
+    //RoadSectionsByCarriageway,
+};
 use crate::util::convert_degrees_to_metres;
 
-use std::collections::BTreeMap;
-use std::collections::btree_map::Entry;
+//use std::collections::BTreeMap;
+//use std::collections::btree_map::Entry;
 
 
 
@@ -31,21 +39,21 @@ pub struct Lookup {
 #[pymethods]
 impl Lookup {
 
-    #[new]
-    pub fn new(py:Python) -> Self {
-        py.run("print('dummy')", None, None).unwrap();
-        Lookup {
-            features: Vec::new(),
-            //index: BTreeMap::new(),
-        }
-    }
+    // #[new]
+    // pub fn new(py:Python) -> Self {
+    //     py.run("print('dummy_conf3')", None, None).unwrap();
+    //     Lookup {
+    //         features: Vec::new(),
+    //         //index: BTreeMap::new(),
+    //     }
+    // }
 
     /// (lat, lon, cwy, network_type, /)
     /// --
     ///
     /// A basic one-at-a-time lookup of the closest feature to the given lat/lon
     /// May be slow for bulk lookups
-    #[pyo3(text_signature = "(lat, lon, cwy, network_type, /)")]
+    //#[pyo3(text_signature = "($self, lat, lon, carriageways, network_type, /)")]
     pub fn road_slk_from_coordinate(
         &self,
         lat: f64,
@@ -136,7 +144,6 @@ impl Lookup {
     //             Some(PyList::new(py, list_of_points).to_object(py))
     //         }).collect();
     //     Ok(PyList::new(py, list_of_lists).into())
-        
     // }
 
     // pub fn to_binary(&self, py: Python) -> PyResult<PyObject> {
@@ -157,72 +164,57 @@ impl Lookup {
     // }
 
     #[staticmethod]
-    pub fn from_dict(python_dictionary: &PyDict, py:Python) -> PyResult<Self> {
-        py.run("print('extracting features key')", None, None)?;
-        Ok(Self::new(py))
+    //#[pyo3(text_signature = "(python_dictionary,/)")]
+    pub fn from_dict(python_dictionary: &PyDict) -> PyResult<Self> {
+        // py.run("print('extracting features key')", None, None)?;
+
+        let mut features: Vec<ExtractedFeature> = python_dictionary
+            .get_item("features")
+            .unwrap()
+            .extract::<Vec<&PyAny>>()?
+            .into_iter()
+            .map(|feature| feature.extract::<ExtractedFeature>().unwrap())
+            .collect();
+
+        // let python_dictionary:&PyDict = match python_dictionary.extract(){
+        //     Ok(python_dictionary) => python_dictionary,
+        //     Err(_) => return Err(PyValueError::new_err("Function takes dictionary as argument"))
+        // };
 
         // let features = match python_dictionary.get_item("features"){
-        //     Some(features) => Ok(features),
-        //     None=>Err(PyValueError::new_err("No features found in dictionary"))
-        // }?;
+        //     Some(features) => features,
+        //     None=> return Err(PyValueError::new_err("No features found in dictionary"))
+        // };
+
         // py.run("print('extracting list')", None, None)?;
 
-        // let features =  match features.cast_as::<PyList>(){
-        //     Ok(features) => Ok(features),
-        //     Err(_)=>Err(PyValueError::new_err("Features must be a list"))
-        // }?;
+        // // let features =  match features.cast_as::<PyList>(){
+        // //     Ok(features) => features,
+        // //     Err(_) => return Err(PyValueError::new_err("Features must be a list"))
+        // // };
+
         // py.run("print('Extracting ExtractedFeature')", None, None)?;
-        // let features = features.extract::<Vec<ExtractedFeature>>()?;
+        // let features = match features.extract::<Vec<ExtractedFeature>>(){
+        //     Ok(features) => features,
+        //     Err(_) => return Err(PyValueError::new_err("`features` must be a list of ExtractedFeature"))
+        // };
 
         
         // Notes:
         // - .sort() is stable; order of equal elements is preserved.
         // - The order of input elements is currently unknown.
         // - the sorted Ord trait of ExtractedFeature currently does not look at slk or true.
+        // - we do still need to sort by road and cwy thats what this does;
+        
         //features.sort(); 
 
-        //let mut index = BTreeMap::new();
+        // let index = Self::build_index(&features);
+        //let index = BTreeMap::new();
         
-        
-
-        // result.insert("".into(), RoadSectionsByCarriageway::new(Some((1,2)), None, None));
-        // return result;
-
-        // let mut current_slice_start = 0;
-
-        // for i in 1..20 {
-        //     let a_feature = &features[i-1];
-        //     let b_feature = &features[i];
-        //     let b_feature_is_new_road = a_feature.properties.road != b_feature.properties.road;
-        //     let b_feature_is_new_cwy  = a_feature.properties.cwy  != b_feature.properties.cwy;
-            
-        //     if b_feature_is_new_road || b_feature_is_new_cwy {
-        //         match index.entry(b_feature.properties.road.clone()) {
-        //             Entry::Vacant(e) => {
-        //                 e.insert(RoadSectionsByCarriageway::new_from_cwy(
-        //                     &a_feature.properties.cwy,
-        //                     (current_slice_start, i),
-        //                 ));
-        //             }
-        //             Entry::Occupied(mut e) => {
-        //                 e.insert(
-        //                     e
-        //                     .get()
-        //                     .with_updated_cwy(
-        //                         &a_feature.properties.cwy,
-        //                          (current_slice_start, i)
-        //                     ),
-        //                 );
-        //             }
-        //         }
-        //         current_slice_start = i;
-        //     }
-        // }
-        
-        // Ok(Self {
-        //     features,
-        //     // index
-        // })
+        Ok(Self {
+            features,
+            //index
+        })
     }
 
 
@@ -234,45 +226,50 @@ impl Lookup {
     //     Ok(index_dict.to_object(py))
     // }
 
-    // fn build_index(features: & Vec<ExtractedFeature>) -> BTreeMap<String, RoadSectionsByCarriageway> {
-    //     let mut index = BTreeMap::new();
-        
-        
-
-    //     // result.insert("".into(), RoadSectionsByCarriageway::new(Some((1,2)), None, None));
-    //     // return result;
-
-    //     let mut current_slice_start = 0;
-
-    //     for i in 1..20 {
-    //         let a_feature = &features[i-1];
-    //         let b_feature = &features[i];
-    //         let b_feature_is_new_road = a_feature.properties.road != b_feature.properties.road;
-    //         let b_feature_is_new_cwy  = a_feature.properties.cwy  != b_feature.properties.cwy;
-            
-    //         if b_feature_is_new_road || b_feature_is_new_cwy {
-    //             match index.entry(b_feature.properties.road.clone()) {
-    //                 Entry::Vacant(e) => {
-    //                     e.insert(RoadSectionsByCarriageway::new_from_cwy(
-    //                         &a_feature.properties.cwy,
-    //                         (current_slice_start, i),
-    //                     ));
-    //                 }
-    //                 Entry::Occupied(mut e) => {
-    //                     e.insert(
-    //                         e
-    //                         .get()
-    //                         .with_updated_cwy(
-    //                             &a_feature.properties.cwy,
-    //                              (current_slice_start, i)
-    //                         ),
-    //                     );
-    //                 }
-    //             }
-    //             current_slice_start = i;
-    //         }
-    //     }
-    //     index
-    // }
+    
         
 }
+
+
+// impl Lookup{
+//     fn build_index(features: & Vec<ExtractedFeature>) -> BTreeMap<String, RoadSectionsByCarriageway> {
+//         let mut index = BTreeMap::new();
+        
+        
+
+//         // result.insert("".into(), RoadSectionsByCarriageway::new(Some((1,2)), None, None));
+//         // return result;
+
+//         let mut current_slice_start = 0;
+
+//         for i in 1..20 {
+//             let a_feature = &features[i-1];
+//             let b_feature = &features[i];
+//             let b_feature_is_new_road = a_feature.properties.road != b_feature.properties.road;
+//             let b_feature_is_new_cwy  = a_feature.properties.cwy  != b_feature.properties.cwy;
+            
+//             if b_feature_is_new_road || b_feature_is_new_cwy {
+//                 match index.entry(b_feature.properties.road.clone()) {
+//                     Entry::Vacant(e) => {
+//                         e.insert(RoadSectionsByCarriageway::new_from_cwy(
+//                             &a_feature.properties.cwy,
+//                             (current_slice_start, i),
+//                         ));
+//                     }
+//                     Entry::Occupied(mut e) => {
+//                         e.insert(
+//                             e
+//                             .get()
+//                             .with_updated_cwy(
+//                                 &a_feature.properties.cwy,
+//                                  (current_slice_start, i)
+//                             ),
+//                         );
+//                     }
+//                 }
+//                 current_slice_start = i;
+//             }
+//         }
+//         index
+//     }
+// }
